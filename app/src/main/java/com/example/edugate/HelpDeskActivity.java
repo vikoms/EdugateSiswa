29720ac1d37d9e23ca41b269b243623b0e57d2b1
Dearavena <dearavena123@gmail.com>
2020-01-29 13:08:50 +0700

    membuat fitur help desk

---
+++ app/src/main/java/com/example/edugate/HelpDeskActivity.java
@@ -0,0 +1,14 @@
+package com.example.edugate;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HelpDeskActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acivity_help_desk);
+    }
+}
